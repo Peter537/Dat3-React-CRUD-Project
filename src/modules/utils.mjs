e0ba65afe2f53b79cdcbfa534748dad2872adedf
TheLonelyDude <magnus.kongstad@gmail.com>
2023-11-22 14:43:24 +0100

    This didnt work. Made it work by adding async tags

--- src/modules/utils.mjs
+++ src/modules/utils.mjs
@@ -1,26 +1,26 @@
-export function fetchData(url, callback, method, body) {
-	const headers = { Accept: "application/json" };
-	if (method === "POST" || method === "PUT") {
-		headers["Content-Type"] = "application/json";
-	}
+export async function fetchData(url, callback, method, body) {
+  const headers = { Accept: "application/json" };
+  if (method === "POST" || method === "PUT") {
+    headers["Content-Type"] = "application/json";
+  }
 
-	const options = {
-		method: method || "GET",
-		headers: headers,
-	};
+  const options = {
+    method: method || "GET",
+    headers: headers,
+  };
 
-	if (body) {
-		options.body = JSON.stringify(body);
-	}
+  if (body) {
+    options.body = JSON.stringify(body);
+  }
 
-	fetch(url, options)
-		.then((response) => response.json())
-		.then((data) => callback(data))
-		.catch((error) => {
-			if (error.status) {
-				error.fullError.then((e) => console.log(e.detail));
-			} else {
-				console.log("Network error");
-			}
-		});
+  await fetch(url, options)
+    .then((response) => response.json())
+    .then((data) => callback(data))
+    .catch((error) => {
+      if (error.status) {
+        error.fullError.then((e) => console.log(e.detail));
+      } else {
+        console.log("Network error");
+      }
+    });
 }
