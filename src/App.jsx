bbcb3a856fd640f4412ce9b05061246ea91eaa12
TheLonelyDude <magnus.kongstad@gmail.com>
2023-11-22 12:52:44 +0100

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import "./styles/App.css";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-	return (
-		<div>
-			<h1>Hello World</h1>
-		</div>
-	);
+  return (
+    <div>
+      <h1>Hello World</h1>
+    </div>
+  );
 }
 
 export default App;
