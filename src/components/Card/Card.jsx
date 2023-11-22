880db9dcd7bc8568471d3f734294c5c4f04d4079
TheLonelyDude <magnus.kongstad@gmail.com>
2023-11-22 22:46:28 +0100

    Improved Card making

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,4 +1,5 @@
 import img from "./placeholder.svg";
+import { HealthIcon, AttackIcon } from "./CardIcons";
 <link
   rel="stylesheet"
   href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.2/font/bootstrap-icons.min.css"
@@ -17,30 +18,10 @@ function Card({ data }) {
           alt="..."
         ></img>
         <div className="mt-2">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            fill="currentColor"
-            className="bi bi-heart-fill"
-            viewBox="0 0 16 16"
-          >
-            <path
-              fillRule="evenodd"
-              d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"
-            />
-          </svg>
+          <HealthIcon />
           <label>{data?.health || 0}</label>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            width="24"
-            height="24"
-            fill="currentColor"
-            className="bi bi-scissors"
-            viewBox="0 0 16 16"
-          >
-            <path d="M3.5 3.5c-.614-.884-.074-1.962.858-2.5L8 7.226 11.642 1c.932.538 1.472 1.616.858 2.5L8.81 8.61l1.556 2.661a2.5 2.5 0 1 1-.794.637L8 9.73l-1.572 2.177a2.5 2.5 0 1 1-.794-.637L7.19 8.61zm2.5 10a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0m7 0a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0" />
-          </svg>
+
+          <AttackIcon />
           <label>{data?.attack || 0}</label>
         </div>
 
--- src/pages/CardMaker.jsx
+++ src/pages/CardMaker.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import CardGrid from "../components/Card/CardGrid";
 import img from "../components/Card/placeholder.svg";
+import { AttackIcon, HealthIcon } from "../components/Card/CardIcons";
 
 function CardMaker() {
   const [image, setImage] = useState(img);
@@ -26,34 +27,14 @@ function CardMaker() {
               ></input>
             </div>
             <div className="form-group mt-2">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                fill="currentColor"
-                className="bi bi-heart-fill"
-                viewBox="0 0 16 16"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M8 1.314C12.438-3.248 23.534 4.735 8 15-7.534 4.736 3.562-3.248 8 1.314"
-                />
-              </svg>
+              <HealthIcon />
               <input
                 type="number"
                 className="form-control form-control-sm text-center"
                 style={{ width: "20%", display: "inline" }}
               ></input>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                fill="currentColor"
-                className="bi bi-scissors"
-                viewBox="0 0 16 16"
-              >
-                <path d="M3.5 3.5c-.614-.884-.074-1.962.858-2.5L8 7.226 11.642 1c.932.538 1.472 1.616.858 2.5L8.81 8.61l1.556 2.661a2.5 2.5 0 1 1-.794.637L8 9.73l-1.572 2.177a2.5 2.5 0 1 1-.794-.637L7.19 8.61zm2.5 10a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0m7 0a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0" />
-              </svg>
+
+              <AttackIcon />
               <input
                 type="number"
                 className="form-control form-control-sm text-center"
