530f1a35ed1ad6b3ac7809e2f00e8f58dbcc4a8a
TheLonelyDude <magnus.kongstad@gmail.com>
2023-11-22 18:35:35 +0100

    Create CardGrid.jsx

---
+++ src/components/Card/CardGrid.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Card from "./Card";
+// https://getbootstrap.com/docs/5.3/components/card/#grid-cards
+function CardGrid({ cards }) {
+  return (
+    <div className="row row-cols-2 row-cols-md-4 g-2">
+      {cards.map((card, index) => {
+        return (
+          <div className="col">
+            {" "}
+            <Card key={index} {...card} />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default CardGrid;
