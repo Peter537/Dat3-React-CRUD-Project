ecae063dc87cd54a3884b101e6b5b07081c98611
TheLonelyDude <magnus.kongstad@gmail.com>
2023-11-22 13:44:57 +0100

    Added imports and login api

--- src/api/api.js
+++ src/api/api.js
@@ -1,5 +1,23 @@
 // API
+import { fetchData } from "../modules/utils.mjs";
+
+const BASE_URL = "http://localhost:3001/";
 
 // #region Login
+
+function login(username, password) {
+  const url = `${BASE_URL}login`;
+  const method = "POST";
+  const body = { username, password };
+
+  fetchData(
+    url,
+    (data) => {
+      console.log(data);
+    },
+    method,
+    body
+  );
+}
 
 // #endregion
