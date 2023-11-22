ba7b40f619ed3b435e7600178a13fc2ab0eb8810
TheLonelyDude <magnus.kongstad@gmail.com>
2023-11-22 14:43:09 +0100

    Added login calls

--- src/api/api.js
+++ src/api/api.js
@@ -5,19 +5,47 @@ const BASE_URL = "http://localhost:3001/";
 
 // #region Login
 
-function login(username, password) {
-  const url = `${BASE_URL}login`;
-  const method = "POST";
-  const body = { username, password };
+async function getAllUsers() {
+  const url = `${BASE_URL}user`;
+  let users = [];
+  await fetchData(url, (data) => {
+    users = data;
+  });
 
-  fetchData(
+  return users;
+}
+
+export async function login(name, password) {
+  const url = `${BASE_URL}user`;
+  let users = [];
+  await fetchData(url, (data) => {
+    users = data;
+  });
+
+  return users.find((user) => user.name === name && user.password === password);
+}
+
+export async function register(name, password) {
+  const users = await getAllUsers();
+  if (users.find((user) => user.name === name)) {
+    alert("Username already taken");
+    return null;
+  }
+  const url = `${BASE_URL}user`;
+  const method = "POST";
+  const body = { name, password };
+  let user = null;
+
+  await fetchData(
     url,
     (data) => {
-      console.log(data);
+      user = data;
     },
     method,
     body
   );
+
+  return user;
 }
 
 // #endregion
