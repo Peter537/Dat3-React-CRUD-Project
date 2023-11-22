45a1a048754b4634c271bf0eb211d7b200d06f94
TheLonelyDude <magnus.kongstad@gmail.com>
2023-11-22 13:13:13 +0100

    Added routes

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,23 @@ import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./pages/Login";
 import CardMaker from "./pages/CardMaker";
+import Header from "./components/Header";
 
 function App() {
+  document
+    .getElementsByTagName("body")[0]
+    .setAttribute("data-bs-theme", "dark");
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Login />} />
-        <Route path="/maker" element={<CardMaker />} />
-      </Routes>
-    </BrowserRouter>
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Login />} />
+          <Route path="/maker" element={<CardMaker />} />
+        </Routes>
+      </BrowserRouter>
+      <Header />
+    </>
   );
 }
 
