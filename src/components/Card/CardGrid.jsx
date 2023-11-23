be8a8cdacbd2c61ab46866e2b9351df020b3933c
TheLonelyDude <magnus.kongstad@gmail.com>
2023-11-23 23:41:37 +0100

    Implemented move cards

--- src/components/Card/CardGrid.jsx
+++ src/components/Card/CardGrid.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Card from "./Card";
+import "../../../node_modules/jquery/dist/jquery.js";
+import "../../../node_modules/jquery-ui/dist/jquery-ui.js";
 
 // https://getbootstrap.com/docs/5.3/components/card/#grid-cards
 function CardGrid({ cards, click, button_text, max_cards = 100 }) {
+  $("#card-grid").sortable();
   return (
     <div
       className="row row-cols-2 row-cols-sm-4 g-2 justify-content-start"
       style={{ minWidth: "var(--bs-breakpoint-xl)" }}
+      id="card-grid"
     >
       {cards.slice(0, max_cards).map((card, index) => {
         return (
