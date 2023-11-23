fa8cdcc03b40f4a0746a1222cda8a6ec4d67a7d4
TheLonelyDude <magnus.kongstad@gmail.com>
2023-11-23 09:46:11 +0100

    Fixed data point error

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -31,7 +31,7 @@ function Card({ data }) {
         </div>
 
         <div className="card-body">
-          <h5 className="card-title">{data?.title || "Card title"}</h5>
+          <h5 className="card-title">{data?.name || "Card title"}</h5>
           <p className="card-text">
             {data?.desc ||
               "Some quick example text to build on the card title and make up the bulk of the card's content."}
