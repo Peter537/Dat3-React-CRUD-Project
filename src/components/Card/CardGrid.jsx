d25ed1cfc578e901c6f87cfc978997604a0ba197
TheLonelyDude <magnus.kongstad@gmail.com>
2023-11-23 23:30:29 +0100

    Made grid start from the left as a starter

--- src/components/Card/CardGrid.jsx
+++ src/components/Card/CardGrid.jsx
@@ -1,9 +1,13 @@
 import React from "react";
 import Card from "./Card";
+
 // https://getbootstrap.com/docs/5.3/components/card/#grid-cards
 function CardGrid({ cards, click, button_text, max_cards = 100 }) {
   return (
-    <div className="row row-cols-2 row-cols-sm-4 g-2">
+    <div
+      className="row row-cols-2 row-cols-sm-4 g-2 justify-content-start"
+      style={{ minWidth: "var(--bs-breakpoint-xl)" }}
+    >
       {cards.slice(0, max_cards).map((card, index) => {
         return (
           <div className="col col-sm-auto" key={index}>
