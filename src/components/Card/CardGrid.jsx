b3f9052b850bbee977fb242998488c4f4cd6304f
TheLonelyDude <magnus.kongstad@gmail.com>
2023-11-23 10:06:36 +0100

    Update CardGrid.jsx

--- src/components/Card/CardGrid.jsx
+++ src/components/Card/CardGrid.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import Card from "./Card";
 // https://getbootstrap.com/docs/5.3/components/card/#grid-cards
-function CardGrid({ cards }) {
+function CardGrid({ cards, click, button_text }) {
   return (
     <div className="row row-cols-2 row-cols-md-4 g-2">
       {cards.map((card, index) => {
         return (
           <div className="col" key={index}>
             {" "}
-            <Card key={index} data={card} />
+            <Card
+              key={index}
+              data={card}
+              click={() => click(card.id)}
+              button_text={button_text}
+            />
           </div>
         );
       })}
