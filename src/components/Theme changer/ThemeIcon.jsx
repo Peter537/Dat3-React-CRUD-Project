e946885da68d7338076ae76a52edb769a2555f9f
TheLonelyDude <magnus.kongstad@gmail.com>
2023-12-02 13:27:14 +0100

    Added example of lifting state
    https://react.dev/learn/sharing-state-between-components

--- src/components/Theme changer/ThemeIcon.jsx
+++ src/components/Theme changer/ThemeIcon.jsx
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState } from "react";
 import Day from "./Day";
 import Night from "./Night";
 
 function ThemeIcon() {
-  const theme = sessionStorage.getItem("theme");
+  const [theme, setTheme] = useState(sessionStorage.getItem("theme") || "dark");
+
+  function changeTheme() {
+    if (theme === "dark") {
+      sessionStorage.setItem("theme", "light");
+      setTheme("light");
+    } else {
+      sessionStorage.setItem("theme", "dark");
+      setTheme("dark");
+    }
+    window.location.reload();
+  }
 
   return (
     <>
-      {theme === "dark" && <Night />}
-      {theme === "light" && <Day />}
+      {theme === "dark" && <Night onChangeTheme={changeTheme} />}
+      {theme === "light" && <Day onChangeTheme={changeTheme} />}
     </>
   );
 }
